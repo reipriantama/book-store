4582a5b802404e31ad6cb41847146722405cbf18
reipriantama <priantamareinato@gmail.com>
2024-02-15 17:32:43 +0700

    add navbar button

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -4,7 +4,7 @@ const Button = ({ className, buttonText }) => {
   return (
     <div>
       <button
-        className={`px-14 py-5 bg-[#FFCA42] text-[19px] font-cardo ${className}`}
+        className={` px-14 py-5 bg-[#FFCA42] text-[19px] font-cardo ${className}`}
       >
         <div>{buttonText}</div>
       </button>

d24012c4c8e202ec1ffa35245e1563d11f178f39
reipriantama <priantamareinato@gmail.com>
2024-02-16 14:53:19 +0700

    add feature section2

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../../components/features/hero';
 import Author from '../../components/features/section1';
+import About from '../../components/features/section2';
 
 const LandingPage = () => {
   return (
     <div>
       <Hero />
       <Author />
+      <About />
     </div>
   );
 };

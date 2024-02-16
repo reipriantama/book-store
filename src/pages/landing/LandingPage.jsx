cb92951a3131b5438070f26e0b2de57a8d6c166d
reipriantama <priantamareinato@gmail.com>
2024-02-16 14:07:10 +0700

    add section1

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../../components/features/hero';
+import Author from '../../components/features/section1';
 
 const LandingPage = () => {
   return (
     <div>
       <Hero />
+      <Author />
     </div>
   );
 };

44b5675809d40ae2e50306405b94de64a01a3cfd
reipriantama <priantamareinato@gmail.com>
2024-02-16 17:03:26 +0700

    add feature/section4

---
+++ src/components/features/section4/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Callout = () => {
+  return (
+    <div
+      className='font-inter bg-[#1B3764] text-white
+    xl:px-[310px] xl:py-[37px]'
+    >
+      <div className='flex justify-center'>
+        <div>left</div>
+        <div>image</div>
+      </div>
+    </div>
+  );
+};
+
+export default Callout;

14dec8de5f87cd796519171b88d15e82f84dc42b
reipriantama <priantamareinato@gmail.com>
2024-02-19 15:40:52 +0700

    add feature about section1

--- src/pages/aboutPage/AboutPage.jsx
+++ src/pages/aboutPage/AboutPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TitleHero from '../../components/ui/titleHero';
+import AboutAuthor from '../../components/features/about/section1';
 
 const AboutPage = () => {
   return (
@@ -8,6 +9,7 @@ const AboutPage = () => {
         titleText='About the Author'
         descText='There are many variations of passages of Lorem Ipsum available,  have suffered alteration in some form.'
       />
+      <AboutAuthor />
     </div>
   );
 };
